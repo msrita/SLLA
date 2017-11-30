96546226ae0c768c97ac02fd8792206bfd959fe4
Justin <justsamovar@gmail.com>
2017-11-29 19:58:14 -0800

    Basic Implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
+  get '/submit' => 'submit_events#index'
+  post '/submit' => 'submit_events#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
